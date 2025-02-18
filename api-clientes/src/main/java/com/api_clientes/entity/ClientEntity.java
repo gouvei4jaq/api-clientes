ef27d78ba45f5ad3de43e6aba40dffff384ece5a
gouvei4jaq <jaquelinegouvei4@gmail.com>
2025-02-18 18:42:42 -0300

    Retirada de validações na ClientEntity

--- api-clientes/src/main/java/com/api_clientes/entity/ClientEntity.java
+++ api-clientes/src/main/java/com/api_clientes/entity/ClientEntity.java
@@ -14,10 +14,7 @@ import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.validator.constraints.br.CPF;
 
 import java.math.BigDecimal;
@@ -25,6 +22,8 @@ import java.time.LocalDate;
 
 @Data
 @Builder
+@Getter
+@Setter
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
@@ -37,36 +36,24 @@ public class ClientEntity {
     private Long id;
 
     @Column(name = "nome")
-    @NotBlank(message = "Nome é um campo obrigatório")
-    @Size(min = 3, message = "O campo Nome deve ter pelo menos 3 caracteres")
     private String name;
 
     @Column(name = "cpf", unique = true, nullable = false)
-    @CPF(message = "Cpf Inválido")
-    @NotBlank(message = "CPF é um campo obrigatório")
-    @Size(min = 11, max = 11, message = "CPF precisa ter 11 dígitos")
     private String cpf;
 
     @Column(name = "email", unique = true, nullable = false)
-    @Email(message = "Email inválido")
     private String email;
 
     @Column(name = "dataNascimento")
-    @NotNull(message = "Data de nascimento é obrigatória")
-    @Past(message = "A data de nascimento deve estar no passado")
-    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate dateOfBirth;
 
     @Column(name = "telefone")
-    @Pattern(regexp = "\\d{10,11}", message = "Telefone deve ter 10 ou 11 dígitos")
     private String cellPhone;
 
     @Column(name = "endereço")
-    @NotBlank(message = "Endereço é um campo obrigatório")
     private String address;
 
     @Column(name = "saldo")
-    @PositiveOrZero(message = "O campo Saldo não pode ser negativo")
     private BigDecimal balance;
 
 
