75661826e775dc3f1cf1c35535db98c1ea1ae58f
gouvei4jaq <jaquelinegouvei4@gmail.com>
2025-02-18 15:49:23 -0300

    Criação da

---
+++ api-clientes/src/main/java/com/api_clientes/controller/ClientController.java
@@ -0,0 +1,60 @@
+package com.api_clientes.controller;
+
+import com.api_clientes.entity.ClientEntity;
+import com.api_clientes.service.ClientService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/clientes")
+public class ClientController {
+
+    private final ClientService clientService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createClient(@RequestBody @Valid ClientEntity clientEntity) {
+        clientService.createClient(clientEntity);
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientEntity> findClient(@PathVariable Long id){
+        return clientService.findClientById(id)
+                .map(client -> ResponseEntity.ok(client))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ClientEntity> updateClient(
+            @PathVariable Long id, @RequestBody @Valid ClientEntity updateClient){
+       clientService.updateClientById(id, updateClient);
+       return ResponseEntity.ok(updateClient);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteClient(@PathVariable Long id){
+        clientService.deleteClientById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+
+
+
+}
+
+
