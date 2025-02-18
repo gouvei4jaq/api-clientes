67b0bf14c90616682500b9ce487ba40e06488c7c
gouvei4jaq <jaquelinegouvei4@gmail.com>
2025-02-18 15:50:20 -0300

    Criação da ClientService

---
+++ api-clientes/src/main/java/com/api_clientes/service/ClientService.java
@@ -0,0 +1,46 @@
+package com.api_clientes.service;
+
+import com.api_clientes.entity.ClientEntity;
+import com.api_clientes.repository.ClientRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ClientService {
+
+    private final ClientRepository clientRepository;
+
+    @Transactional
+    public ClientEntity createClient (@Valid ClientEntity clientEntity){
+        return clientRepository.save(clientEntity);
+    }
+
+    public Optional<ClientEntity> findClientById (Long id){
+        return clientRepository.findById(id);
+    }
+
+    @Transactional
+    public ClientEntity updateClientById (Long id, @Valid ClientEntity updateClient){
+        return clientRepository.findById(id).map(clientEntity -> {
+            clientEntity.setName(updateClient.getName());
+            clientEntity.setCpf(updateClient.getCpf());
+            clientEntity.setEmail(updateClient.getEmail());
+            clientEntity.setDateOfBirth(updateClient.getDateOfBirth());
+            clientEntity.setCellPhone(updateClient.getCellPhone());
+            clientEntity.setBalance(updateClient.getBalance());
+            return clientEntity;
+        }).orElseThrow(() -> new EntityNotFoundException("Cliente Não Encontrado."));
+    }
+
+    @Transactional
+    public void deleteClientById(Long id){
+        clientRepository.findById(id);
+    }
+
+}
