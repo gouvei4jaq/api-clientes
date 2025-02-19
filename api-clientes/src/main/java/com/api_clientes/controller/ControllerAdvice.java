77cc77ace8aedf863e5db4cd62b651e91417580f
gouvei4jaq <jaquelinegouvei4@gmail.com>
2025-02-19 20:58:17 -0300

    Configuração da ControllerAdvice

--- api-clientes/src/main/java/com/api_clientes/controller/ControllerAdvice.java
+++ api-clientes/src/main/java/com/api_clientes/controller/ControllerAdvice.java
@@ -2,7 +2,10 @@ package com.api_clientes.controller;
 
 
 import com.api_clientes.exception.ClientNotFoundException;
+import com.api_clientes.exception.ClientUnderageException;
+import com.api_clientes.exception.DuplicateCpfException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
@@ -14,7 +17,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
-
+@Slf4j
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class ControllerAdvice {
@@ -25,15 +28,40 @@ public class ControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error ->
-                errors.put(error.getField(), error.getDefaultMessage())
-        );
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String messageCode = error.getDefaultMessage();
+
+            String resolvedMessage = messageSource.getMessage(messageCode, null, messageCode, LocaleContextHolder.getLocale());
+
+            errors.put(error.getField(), resolvedMessage);
+
+        });
+
         return errors;
     }
 
     @ExceptionHandler(ClientNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleClientNotFoundException(ClientNotFoundException ex) {
+        return messageSource.getMessage(ex.getMessage(), null, LocaleContextHolder.getLocale());
+    }
+
+    @ExceptionHandler(ClientUnderageException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handlerClientUnderageException(ClientUnderageException ex) {
+        return messageSource.getMessage(ex.getMessage(), null, LocaleContextHolder.getLocale());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public void handleException(Exception exception) {
+        log.error(exception.getMessage(), exception);
+    }
+
+    @ExceptionHandler(DuplicateCpfException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public String handlerDuplicateCpfException(DuplicateCpfException ex) {
         return messageSource.getMessage(ex.getMessage(), null, LocaleContextHolder.getLocale());
     }
 
