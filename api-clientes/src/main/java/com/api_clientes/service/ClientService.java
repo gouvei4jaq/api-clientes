77d25cc81734e9b58c03ba50141c01c71c5bf516
gouvei4jaq <jaquelinegouvei4@gmail.com>
2025-02-21 17:51:59 -0300

    add logs na ClientService

--- api-clientes/src/main/java/com/api_clientes/service/ClientService.java
+++ api-clientes/src/main/java/com/api_clientes/service/ClientService.java
@@ -7,12 +7,14 @@ import com.api_clientes.entity.ClientEntity;
 import com.api_clientes.exception.ClientNotFoundException;
 import com.api_clientes.repository.ClientRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Period;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ClientService {
@@ -20,30 +22,44 @@ public class ClientService {
     private final ClientRepository clientRepository;
 
     public ClientEntity createClient (ClientRequest clientRequest){
+
+        log.info("Creating client with CPF: {}****", clientRequest.getCpf().substring(0, 4));
+
         validateAge(clientRequest.getDataNascimento());
+
         try {
-            return clientRepository.save(ClientEntity.valueOf(clientRequest));
+            ClientEntity client = clientRepository.save(ClientEntity.valueOf(clientRequest));
+            log.info("Client created successfully with ID: {}", client.getId());
+            return client;
         }catch (DataIntegrityViolationException ex){
+            log.error("Duplicate CPF found: {}", clientRequest.getCpf().substring(0,4));
             throw new DuplicateCpfException("422.003");
         }
     }
 
     public ClientEntity findClientById (Long id){
+        log.info("Searching for client with ID: {}", id);
         return clientRepository.findById(id).orElseThrow(() -> new ClientNotFoundException("404.000"));
     }
 
     public ClientEntity updateClientById (Long id, ClientRequest clientRequest){
+        log.info("Updating client with ID: {}", id);
         validateAge(clientRequest.getDataNascimento());
+
         return clientRepository.findById(id).map(clientEntity -> {
             clientEntity.updateClient(clientRequest);
+            log.info("Client updated successfully!");
             return clientRepository.save(clientEntity);
+
         }).orElseThrow(() -> new ClientNotFoundException("404.000"));
     }
 
     public void deleteClientById(Long id){
+        log.info("Trying delete client with ID: {}", id);
         ClientEntity client = clientRepository.findById(id)
                 .orElseThrow(() -> new ClientNotFoundException("404.000"));
         clientRepository.delete(client);
+        log.info("Client with ID: {} deleted successfully", id);
     }
 
     public void validateAge(LocalDate birthDate){
